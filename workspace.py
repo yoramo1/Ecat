61078dcc95ea7ca7392302959daf94d77288bdbd
yoramo1 <35218697+yoramo1@users.noreply.github.com>
2019-05-29 19:00:50 +0300

    Update workspace.py
    workspace load

--- workspace.py
+++ workspace.py
@@ -19,13 +19,62 @@ class workspace:
 		root = self.tree.getroot()	
 		xml_list = root.findall('Workspace/Targets/item')
 		if  xml_list != None:
-			YoUtil.debug_print('lst',xml_list)
+			#YoUtil.debug_print('lst',xml_list)
 			for xml_node in xml_list:
-				pass
-				
+				tm = target_model(xml_node)
+				self.target_list.append(tm)
 		
 	def to_string(self):
-		ret = 'Workspace'
+		ret = 'Workspace ==>> '
 		for tm in self.target_list:
 			ret += '\n\t'+tm.to_string()
+		return ret
+		
+class target_model:
+	def __init__(self,xml_node):
+		#YoUtil.debug_print(xml_node.tag,None)
+		self.xml_node = xml_node
+		self.item_type = None
+		self.the_xml_node= None
+		self.name=None
+		self.type=None
+		self.id=None	
+		self.ParentGMASName=None
+		self.TargetType=None
+		self.load()
+		
+	def load(self):
+		self.item_type = self.xml_node.find('item_type').text
+		self.shrot_item_type = self.get_short_item_type()
+		if self.shrot_item_type!=None:
+			self.the_xml_node=self.xml_node.find(self.shrot_item_type)
+			if self.the_xml_node!= None:
+				self.name = self.the_xml_node.get('Name')
+				self.ParentGMASName=self.the_xml_node.get('ParentGMASName')
+				self.TargetType = self.the_xml_node.get('TargetType')
+				self.id = self.the_xml_node.get('ID')
+		
+	def get_short_item_type(self ):
+		ret=None
+		if self.item_type!= None:
+			s = self.item_type.split(',')
+			s1=s[0].split('.')
+			l = len(s1)
+			if l > 0:
+				ret= s1[l-1]
+		return ret
+	
+	def to_string(self):
+		ret = 'TargetModel - '
+		if self.shrot_item_type!= None:
+			ret = self.shrot_item_type
+		if self.name!= None:
+			ret += '\tName='+self.name
+		if self.id!= None:
+			ret += '\tID='+self.id
+		if self.ParentGMASName!= None:
+			ret += '\tParent='+self.ParentGMASName
+		if self.TargetType!= None:
+			ret += '\tTargetType='+self.TargetType
+		
 		return ret
